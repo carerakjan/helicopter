e33d9b190947133fd23d3c7941d09e2fb8e7c298
Bo_Hr <carerakjan@gmail.com>
2024-05-07 18:12:48 +0300

    improved menu logic (prevent two win at the same time)

--- main.py
+++ main.py
@@ -28,40 +28,53 @@ class Table(Frame):
         self.pack()
 
 class EditWindow(Toplevel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.protocol('WM_DELETE_WINDOW', self.on_destroy_window)
+    
+    def on_destroy_window(self):
+        self.master.is_edit_dialog_opened = False
+        self.destroy()  
+
     def draw_table(self, data):
         self.table = Table(self)
         self.table.draw_json(data)
         self.mainloop()
 
 class AppMenu(Menu):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, cfg = None, **kwargs):
         super().__init__(*args, **kwargs)
+        self.app_config = cfg
         self.add_items()
+        self.is_edit_dialog_opened  = False
 
     def add_items(self):
         editmenu = Menu(self, tearoff=0)
-        editmenu.add_command(label='Параметры вертольота')
-        editmenu.add_command(label='Постоянная загрузка', command=self.show_edit_dialog)
+        editmenu.add_command(label='Параметры вертольота', command=self.show_edit_helicopter_settings)
+        editmenu.add_command(label='Постоянная загрузка', command=self.show_edit_constant_load)
         self.add_cascade(label='Редактировать', menu=editmenu)
 
-    def show_edit_dialog(self):
-        if self.table_data:
-            EditWindow(self).draw_table(self.table_data)
-        
-    def set_table_data(self, data):
-        self.table_data = data
+    def show_edit_constant_load(self):
+        if self.app_config and not self.is_edit_dialog_opened:
+            self.is_edit_dialog_opened = True
+            EditWindow(self).draw_table(self.app_config['load'])
+
+    def show_edit_helicopter_settings(self):
+        if self.app_config and not self.is_edit_dialog_opened:
+            self.is_edit_dialog_opened = True
+            EditWindow(self)
+    
 
 def main():
     root = Tk()
-    mainmenu = AppMenu(root)
+    cfg = get_config()
+    mainmenu = AppMenu(root, cfg=cfg)
     root.geometry('720x640+240+10')
     root.resizable(False, False)
     root.title('Helicopter')
     root.config(menu=mainmenu)
 
-    cfg = get_config()
     app = App(cfg=cfg)
-    mainmenu.set_table_data(cfg['load'])
     root.mainloop()
 
 if __name__ == '__main__':
